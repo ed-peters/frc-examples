936013944eb45465d078451c61a917a65e87688b
Ed Peters <epeters1111@gmail.com>
2026-01-13 15:47:03 -0500

    Adds config for shooter

--- src/main/java/frc/robot/Config.java
+++ src/main/java/frc/robot/Config.java
@@ -192,6 +192,29 @@ public interface Config {
 
 //endregion
 
+//region Shooter----------------------------------------------------------------
+
+    interface Shooter {
+
+        /** Physical properties of the mechanism */
+        double gearRatio = 1.0 / 3.0;
+        double wheelDiameter = 4.0 / 12.0;
+        double wheelCircumference = wheelDiameter * Math.PI;
+
+        /** Feedforward/feedback constants for speed */
+        DoubleSupplier p = pref("ShooterSubsystem/kP", 0.0);
+        DoubleSupplier d = pref("ShooterSubsystem/kD", 0.0);
+        DoubleSupplier v = pref("ShooterSubsystem/kV", 0.0);
+        DoubleSupplier tolerance = pref("ShooterSubsystem/Tolerance", 0.0);
+
+        /** Preset speeds in feet per seconds */
+        DoubleSupplier speed1 = pref("ShooterPresets/Speed1", 10.0);
+        DoubleSupplier speed2 = pref("ShooterPresets/Speed1", 20.0);
+        DoubleSupplier speed3 = pref("ShooterPresets/Speed3", 3.0);
+    }
+
+//endregion
+
 //region Arm -------------------------------------------------------------------
 
     interface Arm {
